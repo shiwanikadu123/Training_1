8fcc8b492ebf6407e648137831578da22b338d59
shiwanikadu123 <shiwanikadu.actowiz@gmail.com>
2025-02-03 14:00:27 +0530

    Create Program4.py

---
+++ Program4.py
@@ -0,0 +1,9 @@
+a = 10/3
+print("a=", a)  //Output: 3.3333333333333335
+
+b = 10 // 3
+print("b =", b)  //Output: 3
+
+c = 10 / 3
+c = round(c, 2)  # Round to 2 decimal places
+print("c =", c)  //Output: 3.33
